370fb3905f56b3e7262f578becabf99d7264e03b
maodo-malick <62646925+maodo-malick@users.noreply.github.com>
2020-06-04 10:26:20 +0000

    Add files via upload

---
+++ Pages/Admin_side.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin_side</title>
+    <link rel="stylesheet" href="../public/css/Admin_side.css">
+</head>
+<body>
+<div class="page">
+         <div class="entete">
+             <div class="image">
+                 <img src="../Images/quiz.jpeg" alt="">
+             </div>
+             <div class="connect">
+                 <button type="submit"  class="bton" id="button" name="button">Déconnexion</button>
+             </div>
+             <p> Parametrer vos Quizz</p>
+         </div>
+         <div class="partie1">
+             <div class="activity">
+                 <div class="info">
+                     <div class="avatar">
+                     </div>
+                 </div>
+                 <div class="lien"></div>
+             </div>
+         </div>
+         <div class="partie2"></div>
+</div>
+</body>
+</html>
---
+++ Pages/Midle.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="public/css/index.css">
+</head>
+<body>
+   <div class="page">
+         <div class="entete">
+             <div class="image">
+                 <img src="../Images/quiz.jpeg" alt="">
+             </div>
+             <p> Le plaisir de jouer</p>
+         </div>
+         <div class="center">
+         
+         <?php require_once("./Pages/connexion.php"); ?>
+         </div>
+   </div>
+   <Script src="public/js/index.js"></Script>
+   <script src="public/js/Rooter.js"></script>
+</body>
+</html>
