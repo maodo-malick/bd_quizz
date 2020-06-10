a3b5344ea502bbddb4f5f3c343890a832ebba5c9
maodo-malick <62646925+maodo-malick@users.noreply.github.com>
2020-06-10 23:26:54 +0000

    Add files via upload

---
+++ Pages/registration.php
@@ -0,0 +1,49 @@
+
+        <div class="form">
+            <form id="form" method="POST" class="forme">
+                    <div class="haut">
+                        <div class="text">
+                        <p>S'inscrire pour jouer </p>
+                        </div>
+                        <div class="avatar" name="avatar" id="Avatar">
+                        </div>
+                        <div class="fichier">
+                            <button type="file" name="fichier" class="bton">Avatar</button>
+                        </div>
+                    </div>
+                    <div class="form-register">
+                    <label for="Prenom">Prenom</label>
+                    <input type="text" name="Prenom" id="Prenom">
+                    <span class="error" id="error"></span>
+                    </div>
+                    <div class="form-register">
+                    <label for="Nom">Nom</label>
+                    <input type="text" name="Nom" id="Nom">
+                    <span class="error" id="error"></span>
+                    </div>
+                    <div class="form-register">
+                    <label for="Login">Login</label>
+                    <input type="text" name="Login" id="Login">
+                    <span class="error" id="error"></span>
+                    </div>
+                    <div class="form-register">
+                    <label for="Password">Password</label>
+                    <input type="password" name="Password" id="Password">
+                    <span class="error" id="error"></span>
+                    </div>
+                    <div class="form-register">
+                    <label for="Confirmation">Confirmation</label>
+                    <input type="password" name="Confirmation" id="Confirmation">
+                    <span class="error" id="error"></span>
+                    </div>
+                    <div class="form-register">
+                    <label for="Profile">Profile</label>
+                    <input type="text" name="Profile" id="Profile">
+                    <span class="error" id="error"></span>
+                    </div>
+                    <div class="submiter">
+                        <input type="submit" name="register" value="Créer un Compte">
+                    </div>
+            </form>
+        </div>            
+    
