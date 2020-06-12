ad83dbabf7e0d34f89532ffd3d9e2e3726a3d8e3
maodo-malick <62646925+maodo-malick@users.noreply.github.com>
2020-06-12 08:48:26 +0000

    Add files via upload

---
+++ Pages/connexion.php
@@ -0,0 +1,24 @@
+<form method="POST" class="form" id="form" action="index.php?action=Connexion">
+    <div class="connexion" id="connexion">
+      <div class="info">
+            <p>Identifiez-vous!</p>
+    </div>
+    <div class="form-connect">
+                    <label for="Login">Login</label>
+                    <input type="text" name="Login" id="Login">
+                    <span class="error"></span>
+    </div>
+    <div class="form-connect " >
+                    <label for="Password">Password</label>
+                    <input type="Password" name="Password" id="Password">
+                    <span class="error"></span>
+</div>
+    
+    <div class="valide">
+        <input type="button" id="Connexion" name="valide" value="Connexion">
+    </div>
+    <div class="lien">
+   <a  id ="inscription" href="#">S'inscrire pour jouer?</a>
+    </div>
+</div>
+</form>
